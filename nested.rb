9de61db3dafea281264de2304651033372d979c8
Renee Nordholm <reneenordholm@gmail.com>
2019-04-29 19:47:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages].flatten
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
